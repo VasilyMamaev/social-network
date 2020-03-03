56da35d12c456923eac5eb94085cbe624457652c
VasilyMamaev <mamaev93@mail.ru>
2020-03-03 11:09:55 +0500

    добавлен компонент Pagination

---
+++ src/components/ui/paginator/paginator.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from 'react'
+import classes from './paginator.module.css'
+
+const Paginator = React.memo(({totalCount, usersAtPageCount, currentPage, onPageClick, portionSize}) => {
+  
+  let pagesCount = Math.ceil(totalCount / usersAtPageCount)
+  let pages = []
+
+  for (let i = 1; i <= pagesCount; i++ ) {
+    pages.push(i)
+  }
+
+  const portionCount = Math.ceil(pagesCount / portionSize)
+  const [portionNumber, setPortionNumber] = useState(1)
+  const leftPortionPage = (portionNumber - 1) * portionSize + 1
+  const rightPortionPage = leftPortionPage + portionSize
+
+  return (
+    <div>
+      {portionNumber > 1 ?
+      <button onClick={() => {setPortionNumber(portionNumber - 1)}}>previous</button>
+      : null}
+      { pages.filter(p => p >= leftPortionPage && p <= rightPortionPage).map((page) => {
+        return <span 
+          key={page}
+          className={page === currentPage ? classes.selectedPage : null}
+          onClick={() => {onPageClick(page)}}
+        >{page}</span>
+        })
+      }
+      {portionCount > portionNumber ?
+      <button onClick={() => {setPortionNumber(portionNumber + 1)}}>next</button>
+      : null}
+    </div>
+  )
+})
+
+export default Paginator
--- src/components/users/users.jsx
+++ src/components/users/users.jsx
@@ -3,48 +3,42 @@ import noAvatarImg from '../../assets/images/no-avatar-img.jpg'
 import classes from './users.module.css'
 import User from './user/user'
 import Loader from '../ui/loader/loader'
+import Paginator from '../ui/paginator/paginator'
 
 
 function Users(props) {
 
-  let pagesCount = Math.ceil(props.totalCount / props.usersAtPageCount)
-  let pages = []
-
-  for (let i = 1; i <= pagesCount; i++ ) {
-    pages.push(i)
-  }
-
   return (
     <div>
-      { props.isFetching ? <Loader/> : <>
-      <div>
-        { pages.map((page) => {
-          return <span 
-            key={page}
-            className={page === props.currentPage ? classes.selectedPage : null}
-            onClick={() => {props.onPageClick(page)}}
-          >{page}</span>
-        }) }
-      </div>
+      { props.isFetching ? <Loader/> 
+      : <>
+      <Paginator 
+        totalCount={props.totalCount}
+        usersAtPageCount={props.usersAtPageCount}
+        currentPage={props.currentPage}
+        onPageClick={props.onPageClick}
+        portionSize={5}
+      />
       <div>
         { props.users.map(user => <User 
-    key={user.id}
-    id={user.id}
-    followHandler={props.followHandler}
-    avatar={(user.photos.small !== null) ? user.photos.small : noAvatarImg }
-    followed={user.followed}
-    followInProgress={props.followInProgress}
-    name={user.name}
-    userStatus={user.status}
-    city={'user.location.city'}
-    country={'user.location.country'} 
-
-  />) }
+          key={user.id}
+          id={user.id}
+          followHandler={props.followHandler}
+          avatar={(user.photos.small !== null) ? user.photos.small : noAvatarImg }
+          followed={user.followed}
+          followInProgress={props.followInProgress}
+          name={user.name}
+          userStatus={user.status}
+          city={'user.location.city'}
+          country={'user.location.country'} 
+          />) 
+        }
       </div>
       <div>
         <button onClick={() => props.addUsersHandler()}>Show more</button>
       </div>
-</>}
+      </>
+      }
     </div>
   )
 }
